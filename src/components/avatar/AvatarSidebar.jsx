32c93c6b4b5ce3ef88903579ce09f901e400ce50
AndyGeek <andy.eulogio@gmail.com>
2020-10-15 20:50:41 -0500

    Improve styles in sidebar menu

--- src/components/avatar/AvatarSidebar.jsx
+++ src/components/avatar/AvatarSidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const AvatarSidebar = () => {
   return (
     <div>
-      <h1>AvatarSidebar</h1>
+      <h1>Avatar</h1>
     </div>
   );
 };
--- src/components/btn-sidebar/BtnSidebar.jsx
+++ src/components/btn-sidebar/BtnSidebar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BtnSidebar.css";
 
 export const BtnSidebar = (props) => {
+  
+  const [over, setOver] = useState(false);
+
+  const handleMouseOver = () => {
+    setOver(true);
+  };
+  const handleMouseOut = () => {
+    setOver(false);
+  };
+
   return (
-    <div className="btn-sidebar">
-      <span className="btn-sidebar-icon">{props.icon}</span>
+    <div
+      className="btn-sidebar"
+      onMouseEnter={handleMouseOver}
+      onMouseLeave={handleMouseOut}
+    >
+      <span className={over ? "btn-sidebar-icon active" : "btn-sidebar-icon"}>
+        {props.icon}
+      </span>
       <div className="btn-sidebar-title">{props.children}</div>
     </div>
   );
